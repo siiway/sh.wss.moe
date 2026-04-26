38b8e38d799b82faf17329dd28f73f808a3e4887
RhenCloud <i@rhen.cloud>
2026-04-26 22:54:42 +0800

    Fix curl error message in mmproxy script

--- public/scripts/mmproxy.sh
+++ public/scripts/mmproxy.sh
@@ -7,7 +7,7 @@ echo "Help: curl https://sh.wss.moe/mmproxy.help"
 echo "Contact: https://wyf9.top/c"
 echo ""
 
-command -v curl >/dev/null || { echo "Missing git, please install it first."; exit 1; }
+command -v curl >/dev/null || { echo "Missing curl, please install it first."; exit 1; }
 
 TMPDIR=$(mktemp -d)
 trap 'rm -rf "$TMPDIR"' EXIT
